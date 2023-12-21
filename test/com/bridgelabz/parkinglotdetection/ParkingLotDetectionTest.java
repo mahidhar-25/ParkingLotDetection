9f30c02eab15e78216b38034f7d5e0cb702b2ab2
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-21 17:14:35 +0530

    Added : usecase 1 , added testcase for parking a car

---
+++ test/com/bridgelabz/parkinglotdetection/ParkingLotDetectionTest.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.parkinglotdetection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertTrue;
+
+public class ParkingLotDetectionTest {
+
+    /*
+    @desc : this test case validates whether an user can park or not , if there is available position to park ,
+     park the car
+
+     */
+
+    @Test
+    public void UserCanParkHisCar(){
+        Car car = new Car("car1" , "white" , "BMW" , "largeCar");
+        User user = new User("Mahidhar" , car);
+        ArrayList<ArrayList<Boolean>> CarParkingPositions = CarParking.getCarParkingPositions();
+        OptionalInt rowIndex = IntStream.range(0, CarParkingPositions.size())
+                .filter(i -> CarParkingPositions.get(i).stream().anyMatch(Boolean::booleanValue))
+                .findFirst();
+
+        rowIndex.ifPresent(i -> {
+            int columnIndex = CarParkingPositions.get(i).indexOf(true);
+            user.parkCarAtIndex(i, columnIndex);
+        });
+
+        assertTrue(CarParking.isMyCarParkedInParkingLot("Mahidhar" , "car1"));
+    }
+}
