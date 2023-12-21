63e6c463f9aba69f77c1c705b6e06a6ae4306d36
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-21 16:47:11 +0530

    Intialize parking lot detection problem

---
+++ src/com/bridgelabz/parkinglotdetection/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.parkinglotdetection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("!!! Welcome to parking lot detection !!!");
+    }
+}
